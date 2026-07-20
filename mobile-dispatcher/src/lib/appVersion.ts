818f7699186a710109c0716a8749a44e4209082c
PeeGee1985 <petr.gottstein@gmail.com>
2026-07-20 09:09:20 +0200

    fix: make native chat links clickable

--- mobile-dispatcher/src/lib/appVersion.ts
+++ mobile-dispatcher/src/lib/appVersion.ts
@@ -1,5 +1,5 @@
-export const DISPATCHER_APP_VERSION = "1.3.1";
-export const DISPATCHER_APP_VERSION_CODE = 6;
+export const DISPATCHER_APP_VERSION = "1.3.2";
+export const DISPATCHER_APP_VERSION_CODE = 7;
 
 export const DISPATCHER_UPDATE_MANIFEST_URL =
   "https://raw.githubusercontent.com/peegee1985/macaly-kuryr4-you-dispecink-qxs5v8/main/mobile-dispatcher/release.json";
